bc742a853ebd12773fefa8c67c1db80853c1434a
João Costa <costav1402@gmail.com>
2021-06-03 12:11:42 -0300

    criando o login

---
+++ js/login.js
@@ -0,0 +1,15 @@
+function logar(){
+    var email = document.getElementById('email');
+    var pass = document.getElementById('pass');
+
+    console.log(email.value+pass.value);
+
+    if(email.value == "admin@admin.com" && pass.value == "admin"){
+        alert('Logado!');
+        localStorage.setItem("acesso", true);
+
+        window.location.href = "index.html";
+    } else {
+        alert("Usuário ou senha inválidos!");
+    }
+}
